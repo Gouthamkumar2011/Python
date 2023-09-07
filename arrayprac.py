63fdb29b9fe3e09474c4553026e0a89b4dad3d2b
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-09-07 14:34:32 -0400

    arrays

---
+++ arrayprac.py
@@ -0,0 +1,22 @@
+arr = [1,2,3]
+
+print("The new created Array is: ", end=" ")
+for i in range(0,3):
+    print(arr[i])
+
+arr.append(4)
+
+print("The new appended Array is: ", end=" ")
+for i in range(0,4):
+    print(arr[i])
+
+arr.insert(5,2)
+
+print("The new inserted Array is: ", end=" ")
+for i in range(0,5):
+    print(arr[i])
+
+
+
+
+
